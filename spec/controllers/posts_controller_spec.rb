09c739ccac4e556e2c57a27f83d4742fe11e5e42
yankeexe <yankee.exe@gmail.com>
2017-07-01 00:42:22 +0545

    RSpec Edit Action

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,31 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-	describe "POST #create" do 
-		context "with valid attributes" do
-			it "creates a new post" do 
-				expect {
-					post :create, post: FactoryGirl.attributes_for(:post)
-				}.to change(Post, :count).by(1)
-			end
-			it "redirects the user to home page" do
-				post :create, post: FactoryGirl.attributes_for(:post)
-				expect(response).to redirect_to root_path
-			end
+	describe "GET #edit" do 
+		it "renders the edit template" do 
+			post = FactoryGirl.create(:post)
+			get :edit, id: post.id
+			expect(response).to render_template :edit
 		end
 
-		context "with invalid attributes"
-			it "doesn't create a new post" do 
-				expect {
-					post :create, post: FactoryGirl.attributes_for(:invalid_post)
-				}.to change(Post, :count).by(0)
-			
-			end
-
-			it "renders the new template" do 
-				post :create, post: FactoryGirl.attributes_for(:invalid_post)
-				expect(response).to render_template :new
-			end
-		end
+	end
 	
 end
